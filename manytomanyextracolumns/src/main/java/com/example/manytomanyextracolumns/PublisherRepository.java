b36b18a60779d305ac544a1d6eb09eb3a86dee1b
zadigg <abelkibebe5@gmail.com>
2023-03-08 15:19:36 -0600

    Removed Gradle Configs

--- manytomanyextracolumns/src/main/java/com/example/manytomanyextracolumns/PublisherRepository.java
+++ manytomanyextracolumns/src/main/java/com/example/manytomanyextracolumns/PublisherRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.concurrent.Flow;
 
-public interface PublisherRepository extends JpaRepository<Flow.Publisher,Integer> {
+public interface PublisherRepository extends JpaRepository<Publisher,Integer> {
 }
